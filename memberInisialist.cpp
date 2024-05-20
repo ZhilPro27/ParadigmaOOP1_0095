2938d636a5e4c2f9f4183e6ba65cf20d28673617
ZhilPro27 <zhilalkrisna@gmail.com>
2024-05-20 14:33:52 +0700

    Membuat class mahasiswa

--- memberInisialist.cpp
+++ memberInisialist.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 using namespace std;
+
+class mahasiswa {
+    private:
+        const int id;
+        string nama;
+        float nilai;
+
+    public:
+        mahasiswa(int pId, string pNama, float pNilai): id(pId), nama(pNama), nilai(pNilai){
+            //Member Initialization List
+            //Definisi
+        }
+
+        ~mahasiswa(){
+            cout << "Id    = " << id << endl;
+            cout << "Nama  = " << nama << endl;
+            cout << "Nilai = " << nilai << endl;
+        }
+};
