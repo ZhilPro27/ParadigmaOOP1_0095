caff91f3d7135883ab6b902dd86a64c0c634fb12
ZhilPro27 <zhilalkrisna@gmail.com>
2024-05-20 17:38:02 +0700

    Mendefinisikan method class pasien

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -33,3 +33,15 @@ class dokter {
         void tambahPasien(pasien*);
         void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+}
+
+void pasien::cetakDokter() {
+    cout << "Daftar dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) { //auto digunakan dalam perulangan
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
